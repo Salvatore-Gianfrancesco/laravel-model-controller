e18b07169ffd325557c92c7ad41190afe7445754
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-12-19 16:41:02 +0100

    refactoring style and layout [bonus]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,10 @@
+<?php
+function getVote($vote)
+{
+    return round($vote / 2);
+}
+?>
+
 @extends('layouts.app')
 
 @section('content')
@@ -7,10 +14,21 @@
     <div class="row row-cols-5 g-3">
         @forelse($movies as $movie)
         <div class="col">
-            <div class="card h-100 p-2">
-                <h4>{{$movie->title}}</h3>
-                    <div><strong>Nationality</strong>. {{$movie->nationality}}</div>
-                    <div><strong>Date</strong>. {{$movie->date}}</div>
+            <div class="card h-100">
+                <div class="card-top">
+                    <img src="{{$movie->image}}" alt="{{$movie->title}}" class="w-100">
+                    <p class="bg-dark text-light m-0 p-2">{{$movie->description}}</p>
+                </div>
+
+                <div class="card-body">
+                    <h4 class="card-text">{{$movie->title}}</h4>
+                    <div><strong>Nationality</strong>: {{$movie->nationality}}</div>
+                    <div><strong>Date</strong>: {{$movie->date}}</div>
+                    <div><strong>Vote</strong>:
+                        @for($i = 0; $i < getVote($movie->vote); $i++) <i class="fa-solid fa-star"></i> @endfor
+                            @for($i = getVote($movie->vote); $i < 5; $i++) <i class="fa-regular fa-star"></i> @endfor
+                    </div>
+                </div>
             </div>
         </div>
         @empty
