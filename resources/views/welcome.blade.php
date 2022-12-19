b4e5cd28ddd2835cad0ff6a5a3b43fb134b0fd49
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-12-19 15:08:23 +0100

    added cards in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Welcome to Laravel</h1>
+<div class="container py-5">
+    <h1 class="pb-3">Movie Database</h1>
+
+    <div class="row row-cols-5 g-3">
+        @forelse($movies as $movie)
+        <div class="col">
+            <div class="card h-100 p-2">
+                <h4>{{$movie->title}}</h3>
+                    <div><strong>Nationality</strong>. {{$movie->nationality}}</div>
+                    <div><strong>Date</strong>. {{$movie->date}}</div>
+            </div>
+        </div>
+        @empty
+        <div>No movie founded!</div>
+        @endforelse
+    </div>
+</div>
 @endsection
